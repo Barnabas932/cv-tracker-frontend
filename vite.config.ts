797d4b536654b2c4c0813b9b38e1e643956e811c
Barnabas932 <63295575+Barnabas932@users.noreply.github.com>
2025-12-24 16:13:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: [
+      ".replit.dev",
+      ".riker.replit.dev"
+    ]
+  },
+
   preview: {
     port: 5000,
     host: true
