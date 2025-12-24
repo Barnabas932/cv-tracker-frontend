f6ccb6c33e43a52b3ef977de1c4ee1187ed651c7
Barnabas932 <63295575+Barnabas932@users.noreply.github.com>
2025-12-24 16:29:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // Replit preview hostok engedélyezése
   server: {
     allowedHosts: [
       ".replit.dev",
